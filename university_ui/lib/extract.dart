f9d86af36e027114929e28f8bab8aa75780ecd67
Michael Hunter <mhunter@hunter.org>
2026-04-22 09:07:43 -0600

    New Classes added

--- university_ui/lib/extract.dart
+++
@@ -1,8 +0,0 @@
-import 'package:extractor/extractor.dart';
-
-Future<void> main() async {
-// await Extractor.extract('assets/class_descriptions.json', 'gen/');
-// await Extractor.extract('assets/university_react.yaml', 'gen/u_react');
-// await Extractor.extract('assets/university_gen_v2.yaml', 'gen/u_ui_v2');
- await Extractor.extract('assets/university_gen_v3.yaml', 'gen/u_ui_v3');
-}
